61a01b890d0794067a825488a46549efb882c5ee
gp2112 <me@guip.dev>
2022-07-02 11:59:15 -0300

    ipv6 hosting

--- memewiki/app.py
+++ memewiki/app.py
@@ -4,6 +4,7 @@ from memewiki.routes.imageboard_bp import imageboard_bp
 from memewiki.routes.login_bp import login_bp
 from memewiki.routes.signup_bp import signup_bp
 from memewiki.routes.logout_bp import logout_bp
+from memewiki.routes.meme_bp import meme_bp
 import os
 
 __version__ = '0.0.1'
@@ -15,9 +16,11 @@ app.register_blueprint(imageboard_bp, url_prefix='/board')
 app.register_blueprint(login_bp, url_prefix='/login')
 app.register_blueprint(signup_bp, url_prefix='/signup')
 app.register_blueprint(logout_bp, url_prefix='/logout')
+app.register_blueprint(meme_bp, url_prefix='/memes')
+
 
 app.secret_key = os.urandom(12)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='::', debug=True)
 
